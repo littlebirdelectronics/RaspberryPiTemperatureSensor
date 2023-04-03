921aab540732844f57a1b9bc61f15498d52c8c2e
Marcus S <marcus@schappi.com>
2023-04-03 14:34:12 +1000

    Update install.sh
    corrected per comment by @iam2691  reference #1

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 echo "Installing Temperature sensor"
 cp ./TemperatureIcon.png /usr/share/pixmaps/TemperatureIcon.png
 cp ./TemperatureSensor.bin /usr/local/bin/TemperatureSensor.bin
-cp ./TemperatureSensor.desktop /user/share/applications/TemperatureSensor.desktop
+cp ./TemperatureSensor.desktop /usr/share/applications/TemperatureSensor.desktop
 echo "Done Installing Temperature Sensor App"
 
 
